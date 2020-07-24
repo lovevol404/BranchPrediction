dac596b80ca5e9aa34906e5914bcbdb32d349c15
tianming <tianming@qunhemail.com>
2020-07-24 10:36:11 +0800

    优化

--- BranchPrediction.go
+++ BranchPrediction.go
@@ -3,39 +3,66 @@ package main
 import (
 	"fmt"
 	"math/rand"
-	"sort"
 	"time"
 )
 
-const length  = 10000000
+const length = 1000000
+
 func main() {
-	var nums []int
+	nums := make([]int, length)
 	for i := 0; i < length; i++ {
-		nums =append(nums,rand.Intn(length * 10))
+		nums[i] = rand.Intn(length * 10)
 	}
 
 	indexNum := length * 5
-
-	start := time.Now().Nanosecond()
 	count := 0
-	for _,i := range nums{
-		if i > indexNum{
-			count ++
+
+	start := time.Now().UnixNano()
+	for j := 0; j < 10000; j++ {
+		for i := 0; i < length; i++ {
+			if nums[i] > indexNum {
+				count++
+			}
 		}
 	}
-
-	sort.Ints(nums)
-	end := time.Now().Nanosecond()
+	end := time.Now().UnixNano()
 	fmt.Printf("count: %d,耗时：%d\n", count, (end-start)/1000)
 
-	start = time.Now().Nanosecond()
+	quickSort(nums, 0, len(nums)-1)
 	count = 0
-	for _,i := range nums{
-		if i > indexNum{
-			count ++
+
+	start = time.Now().UnixNano()
+	for j := 0; j < 10000; j++ {
+		for i := 0; i < length; i++ {
+			if nums[i] > indexNum {
+				count++
+			}
 		}
 	}
-	end = time.Now().Nanosecond()
+	end = time.Now().UnixNano()
 	fmt.Printf("count: %d,耗时：%d\n", count, (end-start)/1000)
 
+}
+
+func quickSort(a []int, left int, right int) {
+	if left >= right {
+		return
+	}
+	temp := a[left]
+	start := left
+	stop := right
+	for right != left {
+		for right > left && a[right] >= temp {
+			right--
+		}
+		for left < right && a[left] <= temp {
+			left++
+		}
+		if right > left {
+			a[right], a[left] = a[left], a[right]
+		}
+	}
+	a[right], a[start] = temp, a[right]
+	quickSort(a, start, left)
+	quickSort(a, right+1, stop)
 }
